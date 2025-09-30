50c4314d9db53ab20c2b255ffb16cf56830feef4
Steven Asi <stevnasi@gmail.com>
2025-09-30 20:36:59 +1000

    chore: refactoring

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -2,16 +2,16 @@ const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
   const fetchedUrls = urls.map(async (url) => {
-    const response = await httpGet(url);
-    const data = JSON.parse(response.body);
+    const { body, status } = await httpGet(url);
+    const { message } = JSON.parse(body);
 
 
-    if (response.status === 200) {
-      return { 'Arnie Quote': data.message };
+    if (status === 200) {
+      return { 'Arnie Quote': message };
     }
     
     
-    return { 'FAILURE': data.message };
+    return { 'FAILURE': message };
   });
 
   return Promise.all(fetchedUrls);
