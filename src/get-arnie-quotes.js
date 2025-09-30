34937bf2345c130249f02627b41f3e68b598ceaf
Steven Asi <stevnasi@gmail.com>
2025-09-30 20:33:48 +1000

    feat: implementing get arnie quotes logic

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,20 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const fetchedUrls = urls.map(async (url) => {
+    const response = await httpGet(url);
+    const data = JSON.parse(response.body);
+
+
+    if (response.status === 200) {
+      return { 'Arnie Quote': data.message };
+    }
+    
+    
+    return { 'FAILURE': data.message };
+  });
+
+  return Promise.all(fetchedUrls);
 };
 
 module.exports = {
